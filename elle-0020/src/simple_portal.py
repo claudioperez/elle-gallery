faeac3edb8da6a25d0cb5ed3914d8a648f432a4b
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-05-26 04:20:30 -0500

    updates

---
+++ elle-0020/src/simple_portal.py
@@ -0,0 +1,55 @@
+import anabel.backend as anp
+import anabel as em
+
+import elle.sections
+import elle.beam2d
+geom_template = elle.beam2d.transform_no2(elle.beam2d.geom_no1)
+beam_template = elle.beam2d.resp_no1
+
+# Create model Assembler
+model = em.SkeletalModel(ndm=2,ndf=3)
+
+# Define problem parameters
+tf, bf = model.param("tf","bf")
+bw, tw = 18, 18
+A  = model.expr(lambda tf, bf: bf*tf + bw*tw, tf, bf)
+
+def moi(tf, bf):
+    area = bf*tf + bw *tw
+    y_c = 1/(2*area) * (tw*(bw+tf)**2 + (bf-tw)*tf**2)
+    return bf*tf**3/12.0 + (bf*tf)*(tf*0.5*y_c)**2 + \
+           tw*bw**3/12 + tw*bw*((bw+2*tf)*0.5-y_c)**2
+
+I  = model.expr(moi, tf, bf)
+
+# Define model components
+column_section = {"A": 30**2, "E": 3600.0, "I": 30**4}
+girder_section = {"A": A, "E": 3600.0, "I": I}
+basic_girder  = beam_template(**girder_section)
+basic_column  = beam_template(**column_section)
+
+girder = geom_template(basic_girder)
+column = geom_template(basic_column)
+
+# Set up nodes
+ft = 12
+B, H = 30.*ft, 13.*ft
+model.node("1",  0.,  0.)
+model.node("2",  0.,  H )
+model.node("3", B/2,  H )
+model.node("4",  B ,  H )
+model.node("5",  B ,  0.)
+
+model.beam("a", "1", "2", **column_section, elem=column)
+model.beam("b", "2", "3", **girder_section, elem=girder)
+model.beam("c", "3", "4", **girder_section, elem=girder)
+model.beam("d", "4", "5", **column_section, elem=column)
+
+model.boun("1", [1,1,1])
+model.boun("5", [1,1,1])
+
+model.load("2", 1000.0, dof="x")
+model.load("2",   -2.0, dof="y")
+model.load("4",   -2.0, dof="y")
+
+f = model.compose()
